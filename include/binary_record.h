a2bd6eebb4df2c813203fd880ae0b05a8fee24af
Tushar yadav <tushar10591@gmail.com>
2026-07-01 20:44:09 +0530

    initialisation and binaryrecord_h done

---
+++ include/binary_record.h
@@ -0,0 +1,15 @@
+#ifndef BINARY_RECORD_H
+#define BINARY_RECORD_H
+
+#include<cstdint>
+//memory layout control
+#pragma pack(push,1)
+
+struct RecordHeader{
+    uint32_t keySize;
+    uint32_t valueSize;
+};
+
+#pragma pack(pop)
+
+#endif
---
+++ src/storage.cc
@@ -0,0 +1,7 @@
+#include<fstream>
+#include<string>
+#include "../include/binary_record.h"
+
+void writeRecord(const std::string &filename,const std::string &key,const std::string &value){
+
+}
